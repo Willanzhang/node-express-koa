18bfede3d11a172f2d5c075ebcddd74467d09588
master <631871612@qq.com>
2019-10-16 19:56:02 +0800

    koa2 的使用

---
+++ blog-koa2/routes/blog.js
@@ -0,0 +1,68 @@
+const router = require('koa-router')()
+const {
+  getList,
+  getDetail,
+  newBlog,
+  updateBlog,
+  delBlog
+} = require('../controller/blog');
+const { SuccessModel, ErrorModel } = require('../model/resModel');
+const loginCheck = require('../middleware/loginCheck');
+
+
+// 前缀
+router.prefix('/api/blog')
+
+router.get('/list', async function (ctx, next) {
+  let author = ctx.query.author || ''
+  const keyword = ctx.query.keyword || ''
+
+  if (ctx.query.isadmin) {
+      // 管理员界面
+      if (ctx.session.username == null) {
+          // 未登录
+          ctx.body = new ErrorModel('未登录')
+          return;
+      }
+      // // 强制查询自己的博客
+      author = ctx.session.username
+  }
+
+
+  const listData = await getList(author, keyword)
+  ctx.body = new SuccessModel(listData)
+})
+
+router.get('/detail', async function (ctx, next) {
+  const data = await getDetail(ctx.query.id)
+  ctx.body = new SuccessModel(data)
+})
+
+router.post('/new', loginCheck, async function (ctx, next) {
+  ctx.request.body.author = ctx.session.username
+  const data = await newBlog(ctx.request.body)
+  ctx.body = new SuccessModel(data)
+})
+
+router.post('/update', loginCheck, async function (ctx, next) {
+  const { id } = ctx.query;
+  const val = await updateBlog(id, ctx.request.body)
+  if (val) {
+    ctx.body = new SuccessModel()
+  } else {
+    ctx.body = new ErrorModel('更新博客失败')
+  }
+})
+
+router.post('/del', loginCheck, async function (ctx, next) {
+  const { id } = ctx.query;
+  const author = ctx.session.username;
+  const val = await delBlog(id, author);
+  if (val) {
+    ctx.body = new SuccessModel()
+  } else {
+    ctx.body = new ErrorModel('删除博客失败')
+  }
+})
+
+module.exports = router
