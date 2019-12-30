4464163994ebb108fafa8b76d057e51256b38589
master <631871612@qq.com>
2019-12-30 14:44:35 +0800

    events

--- node-base/events.js
+++ node-base/events.js
@@ -1,13 +1,53 @@
-const EventEmitter = require('events');
+// const EventEmitter = require('events');
 
-class MyEmitter extends EventEmitter {
+// class MyEmitter extends EventEmitter {
 
+// }
+
+// const myEmitter = new MyEmitter();
+
+// myEmitter.on('event', () => {
+//     console.log('触发事件');
+// });
+
+// myEmitter.emit('event');
+
+
+// const http = require('http');
+// const util = require('util');
+// const server = http.createServer((req, res) => {
+//     res.end('123');
+// });
+// server.listen(2222);
+// server.on('connection', (stream) => {
+//     console.log('有链接');
+// })
+
+// console.log(util.inspect(server.listeners('connection')));
+
+
+const { once, EventEmitter } = require('events');
+
+async function run() {
+  const ee = new EventEmitter();
+
+  process.nextTick(() => {
+    ee.emit('myevent', 42);
+  });
+
+  const [value] = await once(ee, 'myevent');
+  console.log(value);
+
+  const err = new Error('错误信息');
+  process.nextTick(() => {
+    ee.emit('error', err);
+  });
+
+  try {
+    await once(ee, 'myevent');
+  } catch (err) {
+    console.log('出错', err);
+  }
 }
 
-const myEmitter = new MyEmitter();
-
-myEmitter.on('event', () => {
-    console.log('触发事件');
-});
-
-myEmitter.emit('event');
+run();
