7dfab76a127f35069d6f7501dbbffba0d85ce708
master <631871612@qq.com>
2020-01-03 17:06:42 +0800

    net 模块

---
+++ node-base/net/net.js
@@ -0,0 +1,17 @@
+const net = require('net');
+
+const server = net.createServer((socket) => {
+    socket.end('再见\n');
+  }).on('error', (err) => {
+    // 处理错误
+    throw err;
+  });
+  
+  // 获取任意未使用的端口。
+  server.listen(() => {
+    console.log('打开服务器', server.address());
+  });
+
+  server.getConnections((err, count) => {
+      console.log(count);
+  })
